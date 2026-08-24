149f0d257fc5004208ea4ace8a48bad793ac259a
edersonsm <edersondesouzamarques@gmail.com>
2026-08-24 16:10:50 -0300

    only one answer open at a time

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 // creates an array with all correspondent elements
 const faqButtons = document.querySelectorAll(".question-button");
 
-faqButtons.forEach(function(button) {
+faqButtons.forEach(function(button, index, arrayButtons) {
     const referentAnswer = button.nextElementSibling;
     const icon = button.querySelector("span");
 
@@ -11,6 +11,12 @@ faqButtons.forEach(function(button) {
         if (!isOpen) {
             referentAnswer.style.display = "block";
             icon.innerText = "−";
+            for (let i = 0; i < arrayButtons.length; i ++) {
+                if ((i != index) && (arrayButtons[i].nextElementSibling.style.display === "block")) {
+                    arrayButtons[i].nextElementSibling.style.display = "none"
+                    arrayButtons[i].querySelector("span").innerText = "+"
+                }
+            }
         } else {
             referentAnswer.style.display = "none";
             icon.innerText = "+";
